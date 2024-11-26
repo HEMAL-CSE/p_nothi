451acdeeb4241e6af9306633ff2652f770058327
unknown <habibur15-3697@diu.edu.bd>
2024-11-26 17:35:34 +0600

    update

--- src/pages/Employee/Employee.js
+++ src/pages/Employee/Employee.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import React, { useState } from 'react'
+
 
 export const Employee = () => {
+
+  const [name, setName] = useState('')
+
   return (
-    <div>Employee</div>
+
+<div className='details'>
+                {/* <h2>Cow Purchase</h2> */}
+                <div className="container-fluid px-5 d-none d-lg-block">
+                    <div className="row gx-5 py-3 align-items-center">
+                        <div className="col-lg-3">
+                            {/* <div className="d-flex align-items-center justify-content-start">
+                              <BsPhoneVibrate className='text-success2 fs-1 me-2' />
+                              <h2 className="mb-0">+012 345 6789</h2>
+                          </div> */}
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="d-flex align-items-center justify-content-center">
+                                <a href="#" className="navbar-brand ms-lg-5">
+                                    <h1 className="m-2 display-4 text-success2"><span className="text-success2">Cow</span> Feeding</h1>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <form>
+                    <label>Name:</label>
+                    <input value={name} onChange={e => setName(e.target.value)} className='input' type='text'/>
+
+                    <button onClick={addData} className='button'>Submit</button>
+
+                </form>
+
+
+  </div>
+
+    
   )
 }

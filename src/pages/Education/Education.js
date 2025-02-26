97ab863cc88ba0045c4b5675325160546a2ff183
Fyaz Rayat <fyazrayat@gmail.com>
2025-02-26 18:22:50 +0600

    c

--- src/pages/Education/Education.js
+++ src/pages/Education/Education.js
@@ -15,6 +15,9 @@ export const Education = () => {
     const [bsc, setbsc] = useState('Bachelor’s Degree')
     const [bscs, setbscs] = useState([
         {
+            name: 'Diploma Degree',
+        },
+        {
             name: 'Master’s Degree',
         },
         {
